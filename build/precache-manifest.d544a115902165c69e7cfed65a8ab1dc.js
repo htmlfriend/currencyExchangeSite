39f568ed835ba9b78b11cbc3346b5580727a89f8
Yurii <43734056+htmlfriend@users.noreply.github.com>
2020-06-24 17:52:55 -0400

    Add files via upload

---
+++ build/precache-manifest.d544a115902165c69e7cfed65a8ab1dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0cfb2ee28a6eb26288abf325566ca1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f4234fde627f5e9c238",
+    "url": "/static/css/main.a949050f.chunk.css"
+  },
+  {
+    "revision": "85d12cc6c8b3de7ad2d1",
+    "url": "/static/js/2.b6be78c8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b6be78c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f4234fde627f5e9c238",
+    "url": "/static/js/main.e658d179.chunk.js"
+  },
+  {
+    "revision": "244e557c1a69e098cbda",
+    "url": "/static/js/runtime-main.5bbaf251.js"
+  }
+]);
---
+++ src/About/About.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./About.css";
+
+class About extends React.Component {
+  render() {
+    return (
+      <div className="about-container">
+        <h1 className="about-title">About us</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam,
+          hic? Debitis cumque earum tempore deserunt! Voluptates illum
+          aspernatur quam quod porro praesentium quos velit nemo itaque aliquid
+          nisi iste incidunt, minima sit autem doloribus accusantium, ratione
+          eius ad, voluptatibus delectus cumque debitis! Hic, dicta
+          voluptatibus!
+        </p>
+      </div>
+    );
+  }
+}
+
+export default About;
